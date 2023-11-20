266d835cdb03873aa718669fe6c2dc959f24d6b2
andrey oskin <rreshrr@gmail.com>
2023-11-20 23:32:14 +0300

    playing with post methods

--- src/main/java/ru/thomaskohouse/ArticleManager/ArticleManagerController.java
+++ src/main/java/ru/thomaskohouse/ArticleManager/ArticleManagerController.java
@@ -3,12 +3,13 @@ package ru.thomaskohouse.ArticleManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -24,6 +25,15 @@ public class ArticleManagerController {
     @Autowired
     ArticleManagerService service;
 
+    @GetMapping("articles/")
+    public String mainPage(){
+        return "redirect:/articles/1";
+    }
+
+    @GetMapping("articles")
+    public String mainOnePage(){
+        return "redirect:/articles/1";
+    }
     @GetMapping("articles/{pageNum}")
     public String articlesPage(Model model, @PathVariable Optional<Integer> pageNum){
         int currentPage = pageNum.orElse(1);
@@ -41,7 +51,7 @@ public class ArticleManagerController {
         return "mainPage";
     }
 
-    @GetMapping("articles/article/{id}")
+    @GetMapping("/article/{id}")
     public String articlePage(@PathVariable Long id, Model model){
         Article article = articlesRepository.findById(id).get();
         User author = article.author;
@@ -57,8 +67,16 @@ public class ArticleManagerController {
         return "greeting";
     }
 
-    @GetMapping("/articles/new")
+    @GetMapping("/article/new")
     public String writeArticle(Model model){
+        model.addAttribute("article", new Article());
         return "newArticle";
+    }
+
+    @PostMapping("/article/create")
+    public ResponseEntity<Article> createArticle(Model model, @ModelAttribute Article article){
+        article.setCreationDateTime(LocalDateTime.now());
+        Article newArticle = articlesRepository.save(article);
+        return new ResponseEntity<>(newArticle, HttpStatus.CREATED);
     }
 }
